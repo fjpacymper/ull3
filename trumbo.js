3d5e5e6a8deed5620c8a24494f04466e88f1176f
fjpacymper <fjpa@cymper.com>
2016-10-19 19:22:34 +0100

    Update trumbo.js

--- trumbo.js
+++ trumbo.js
@@ -6,6 +6,18 @@ $(document).ready(function() {
         // Habitualmente, se indica la ruta del SVG en este parámetro, pero
         // en codepen, simplemente pegaremos el contenido del SVG en la parte
         // del código HTML
+lang: 'es', // Idioma (es.min.js incluido)
+  btns: [
+          ['bold', 'italic', 'underline', 'strikethrough'],
+          ['superscript', 'subscript'],
+          ['justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull'],
+          ['unorderedList', 'orderedList'],
+          ['link', 'insertImage'],
+          ['horizontalRule', 'removeformat'],
+          ['viewHTML'],
+          ['fullscreen']
+        ],
+        
     });
 
     $('#trumbowyg').trumbowyg('html'); // Obtienes el HTML generado
