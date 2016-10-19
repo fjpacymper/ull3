93b23e8fd82db486f8589b0811cec65a489d0a8c
fjpacymper <fjpa@cymper.com>
2016-10-19 18:52:08 +0100

    Update trumbo.js

--- trumbo.js
+++ trumbo.js
@@ -2,7 +2,7 @@
 
 /* Javascript */
 $('.editor').trumbowyg({
-    //svgPath: '/icons/icons.svg', // path of icons.svg
+    svgPath: 'icons.svg', // path of icons.svg
     // Habitualmente, se indica la ruta del SVG en este parámetro, pero
     // en codepen, simplemente pegaremos el contenido del SVG en la parte
     // del código HTML
