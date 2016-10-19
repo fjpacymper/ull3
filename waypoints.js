633794834e8f1d81bfef35c73f6b30e249098834
fjpacymper <fjpa@cymper.com>
2016-10-19 19:15:33 +0100

    Create waypoints.js

---
+++ waypoints.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+        // Nuevo waypoint
+    var waypoint = new Waypoint({
+      // Elemento objetivo
+      element: document.getElementById('tres'),
+      // Función disparada
+      handler: function(direction) {
+        alert('¡Has llegado!');
+      }
+    });
+
+});
