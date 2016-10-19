605283807effbaff82d99c4da85965cab32c72c1
fjpacymper <fjpa@cymper.com>
2016-10-19 19:11:34 +0100

    Create waipoints.js

---
+++ waipoints.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    /* Javascript */
+    $('.editor').trumbowyg({
+        svgPath: 'icons.svg', // path of icons.svg
+        // Habitualmente, se indica la ruta del SVG en este parámetro, pero
+        // en codepen, simplemente pegaremos el contenido del SVG en la parte
+        // del código HTML
+    });
+
+    $('#trumbowyg').trumbowyg('html'); // Obtienes el HTML generado
+});
