dc6461e951d681b4420c70d5e61a1ae92d81ca0d
fjpacymper <fjpa@cymper.com>
2016-10-19 19:51:51 +0100

    Update waypoints.js

--- waypoints.js
+++ waypoints.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
         
+               // Nuevo waypoint
         var waypoint = new Waypoint({
+          // Elemento objetivo
           element: document.getElementById('tres'),
+          // Función disparada
           handler: function(direction) {
-            console.log('¡Has pasado por aquí!');
-            if (direction == 'down') {
-              console.log('¡Hacia abajo!');
-              this.destroy();                     // Sólo lo detectará una vez
-            }
+            alert('¡Has llegado!');
           }
         });
+
 
 });
